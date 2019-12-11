16fa565ce5ff329ced2b404475c3538ac23f7a1f
Ronald Aug <saisenfa@gmail.com>
2019-12-11 18:20:40 +0630

    split single consonant

--- syllable-break.js
+++ syllable-break.js
@@ -8,11 +8,12 @@ document.querySelector(".test").addEventListener("click", e => {
     const ar = '\u1083';
     const kaikurn = '\u1086';
     const tarngkakyan = '\u102E';
+    const consonants = '\u1075|\u1076|\u1004|\u1078|\u101E|\u107A|\u1010|\u1011|\u107C|\u1015|\u107D|\u107E|\u1019|\u101A|\u101B|\u101C|\u101D|\u1081|\u1022';
 
     const toneReg = new RegExp(`[${tones}]`, 'gmi');
     const frontReg = new RegExp(`[${ngaPyet}|${kaikurn}|${ar}|${tarngkakyan}]+[^${tones}]`, 'gmi');
     const backReg = new RegExp(`[${ayeSai}]+[^${tones}|${ar}]`, 'gmi');
-
+    const singleReg = new RegExp(`[${consonants}]+[${consonants}]+[^${ngaPyet}]`,'gmi');
 
     // Add space behind every tones
     if (val.match(toneReg)) {
@@ -21,16 +22,18 @@ document.querySelector(".test").addEventListener("click", e => {
         });
     }
 
-    // Add space between between matched words
+    // Add space between matched words
     function splitMatch(pattern) {
         if (!val.match(pattern)) { return }
         val = val.replace(pattern, (matched) => {
             return matched.split("")[0] + " " + matched.split("")[1];
         });
     }
-
+    
     splitMatch(frontReg);
     splitMatch(backReg);
+    splitMatch(singleReg);
+
 
     document.querySelector(".res").innerHTML = val;
 })
